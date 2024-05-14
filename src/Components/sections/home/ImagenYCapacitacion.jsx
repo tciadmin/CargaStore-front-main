0aeb1cbab3daa4b98a88da1d6eddbbfa78b7780d
brian <brianmunozjob@gmail.com>
2024-05-14 17:26:15 -0300

    Cambiar font size a seccion

--- src/Components/sections/home/ImagenYCapacitacion.jsx
+++ src/Components/sections/home/ImagenYCapacitacion.jsx
@@ -10,7 +10,7 @@ const ImagenYCapacitacion = () => {
                 justifyContent={"flex-end"}
                 spacing={2}  >
                 <Grid item xs={6} display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"flex-end"} >
-                    <Typography xs={6} variant="h4" alignSelf={"flex-start"} textAlign={"start"} color={"primary"} fontWeight={"bold"}> Premio Basc</Typography>
+                    <Typography xs={6} variant="h4" sx={{fontSize: {md:"35px", xs: "20px"  }}} alignSelf={"flex-start"} textAlign={"start"} color={"primary"} fontWeight={"bold"}> Premio Basc</Typography>
                     <Typography xs={6} variant="p" fontWeight={"normal"} textAlign={"start"}> Transcomerinter. Recibió el Premio Basc el 07 de Noviembre del 2014, el Primer Puesto en la Categoría Transporte Terrestre, por lo que nos contragulamos con la empresa por este logro. Felicitamos a todos y cada uno de los colaboradores que hacen la cadena logística, y que son merecedores de esta distinción.</Typography>
                 </Grid>
                 <Grid display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} item xs={6}>
@@ -28,7 +28,7 @@ const ImagenYCapacitacion = () => {
                     <img style={{ borderRadius: "5px" }} width={"100%"} src="/home/capacitaciones.png" alt="" />
                 </Grid>
                 <Grid item xs={12} md={5} display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"flex-end"} >
-                    <Typography xs={2} variant="h4" alignSelf={"flex-start"} textAlign={"start"} color={"primary"} fontWeight={"bold"}>Capacitaciones</Typography>
+                    <Typography xs={2} variant="h4" sx={{fontSize: {md:"35px", xs: "20px"  }}} alignSelf={"flex-start"} textAlign={"start"} color={"primary"} fontWeight={"bold"}>Capacitaciones</Typography>
                     <Typography xs={2} variant="p" fontWeight={"normal"} textAlign={"start"}> Mantenemos constantemente capacitado a  todo nuestro personal, con la finalidad de incentivar el mejoramiento continuo de la empresa y brindar un mejor servicio en el transporte de carga pesada nacional e internacional. Nuestro saber es la seguridad de los clientes que confían en nuestro servicio.</Typography>
                 </Grid>
             </Grid>
