8d0ad055108a525e5c6072f6967f4d5750d35301
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2024-05-17 16:31:08 -0500

    Layout Config

--- src/Layouts/LayoutConfi.jsx
+++ src/Layouts/LayoutConfi.jsx
@@ -1,17 +1,44 @@
-import React from "react";
-import CompNavLanding from "../Components/NavLanding/CompNavLanding";
-import { Stack } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import CompNavLanding from "../Components/NavLanding/CompNavLanding"
+import { Stack, Typography } from "@mui/material"
+import { Outlet } from "react-router-dom"
+import { Colors } from "../Utils/Colors"
+import { useState } from "react"
 
 const LayoutConfi = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    console.log(isOpen)
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <CompNavLanding />
+
       <Stack sx={{ display: "flex", flexDirection: "row" }}>
         {/* Para la barra lateral */}
-        <Stack
-          sx={{ width: "472px", border: "1px solid red", height: "95vh" }}
-        ></Stack>
+        <Stack sx={{ width: "472px", border: "1px solid red", height: "95vh" }}>
+          <Typography
+            onClick={toggleSidebar}
+            sx={{ color: Colors.primary.main }}
+          >
+            Datos personales
+          </Typography>
+          <Typography sx={{ color: Colors.primary.main }}>
+            Datos personales
+          </Typography>
+          <Typography
+            sx={{
+              color: Colors.primary.main,
+              fontSize: "16px",
+              lineHeight: "23px",
+            }}
+          >
+            Datos personales
+          </Typography>
+        </Stack>
+
         {/* para las subRutas */}
         <Stack sx={{ width: "100%", border: "1px solid blue", height: "95vh" }}>
           {/* Outlet muestra las subRutas */}
@@ -19,7 +46,7 @@ const LayoutConfi = () => {
         </Stack>
       </Stack>
     </>
-  );
-};
+  )
+}
 
-export default LayoutConfi;
+export default LayoutConfi
