46718c0189a88b0571aad13af1e15853b715e2b3
Facu-J-Boy <boyfacundo45@gmail.com>
2024-06-10 18:37:45 -0300

    reducer y actions de ordenes

---
+++ src/Redux/Actions/OrderActions/listOrder.js
@@ -0,0 +1,19 @@
+import { axiosInstance } from '../../axiosInstance';
+
+export const LIST_ORDER_PENDING = 'LIST_ORDER_PENDING';
+export const LIST_ORDER_SUCCESS = 'LIST_ORDER_SUCCESS';
+export const LIST_ORDER_FAILURE = 'LIST_ORDER_FAILURE';
+
+export const listOrder = (status, orderType) => {
+  return async (dispatch) => {
+    dispatch({ type: LIST_ORDER_PENDING });
+    try {
+      const response = await axiosInstance.get(
+        `/order/list_order?status=${status}&orderType=${orderType}`
+      );
+      dispatch({ type: LIST_ORDER_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: LIST_ORDER_FAILURE, error: error.message });
+    }
+  };
+};
