51d0d8c684fa1f7169c49807026eaf02c7408e97
samunu <micaolor@gmail.com>
2024-06-13 11:14:29 -0300

    changing user reducer

--- src/Redux/Reducers/userReducer.js
+++ src/Redux/Reducers/userReducer.js
@@ -1,40 +1,175 @@
 import {
-  AUTH_USER,
-  GET_ALL_USERS,
-  GET_USER,
-  PATCH_DRIVER,
-  PATCH_TRUCK,
-  POST_USER,
-  PUT_CUSTOMER,
+  AUTH_USER_SUCCESS,
+  AUTH_USER_FAILURE,
+  AUTH_USER_PENDING,
+  GET_ALL_USERS_FAILURE,
+  GET_ALL_USERS_SUCCESS,
+  GET_ALL_USERS_PENDING,
+  GET_USER_FAILURE,
+  GET_USER_PENDING,
+  GET_USER_SUCCESS,
+  PATCH_DRIVER_FAILURE,
+  PATCH_DRIVER_PENDING,
+  PATCH_DRIVER_SUCCESS,
+  PATCH_TRUCK_FAILURE,
+  PATCH_TRUCK_PENDING,
+  PATCH_TRUCK_SUCCESS,
+  POST_USER_FAILURE,
+  POST_USER_PENDING,
+  POST_USER_SUCCESS,
+  PUT_CUSTOMER_FAILURE,
+  PUT_CUSTOMER_PENDING,
+  PUT_CUSTOMER_SUCCESS,
 } from "../Actions/UserActions/userActions";
 
 const initialState = {
   allUsers: [],
   user: [],
+  userLoading: false,
+  error: null,
 };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_ALL_USERS:
-      return { ...state, allUsers: state.allUsers };
+    case GET_ALL_USERS_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case GET_ALL_USERS_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case GET_ALL_USERS_SUCCESS:
+      return {
+        ...state,
+        allUsers: state.allUsers,
+        userLoading: false,
+        error: null,
+      };
 
-    case GET_USER:
-      return { ...state, user: action.paylad };
+    case GET_USER_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case GET_USER_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case GET_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        userLoading: false,
+        error: null,
+      };
 
-    case POST_USER:
-      return { ...state, allUsers: [...state.allUsers, action.payload] };
+    case POST_USER_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case POST_USER_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case POST_USER_SUCCESS:
+      return {
+        ...state,
+        allUsers: [...state.allUsers, action.payload],
+        userLoading: false,
+        error: null,
+      };
 
-    case AUTH_USER:
-      return { ...state, user: action.paylad };
+    case AUTH_USER_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case AUTH_USER_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case AUTH_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        userLoading: false,
+        error: null,
+      };
 
-    case PUT_CUSTOMER:
-      return { ...state, user: action.paylad };
+    case PUT_CUSTOMER_USER_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case PUT_CUSTOMER_USER_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case PUT_CUSTOMER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        userLoading: false,
+        error: null,
+      };
 
-    case PATCH_DRIVER:
-      return { ...state, user: action.paylad };
+    case PATCH_DRIVER_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case PATCH_DRIVER_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case PATCH_DRIVER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        userLoading: false,
+        error: null,
+      };
 
-    case PATCH_TRUCK:
-      return { ...state, user: action.paylad };
+    case PATCH_TRUCK_PENDING:
+      return {
+        ...state,
+        userLoading: true,
+        error: null,
+      };
+    case PATCH_TRUCK_FAILURE:
+      return {
+        ...state,
+        userLoading: false,
+        error: action.error,
+      };
+    case PATCH_TRUCK_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        userLoading: false,
+        error: null,
+      };
 
     default:
       return { ...state };
