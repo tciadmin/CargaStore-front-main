dc9a60d2e117719d567ff0d996be679bdc3c46d7
brianmunioz <brianmunozjob@gmail.com>
2024-07-03 16:28:33 -0300

    Agregar validaciones de campos y mensajes de errores y exito

--- src/Components/inputs/InputForm.jsx
+++ src/Components/inputs/InputForm.jsx
@@ -1,7 +1,8 @@
 import { Box, InputAdornment, MenuItem, OutlinedInput, Select, Typography } from '@mui/material'
 
 // eslint-disable-next-line react/prop-types
-const InputForm = ({value="",setter,label, type = "text",cambiar = false, select = [], money=false, sizeH = "50px",inputW = "100%", marginB = 5, marginT = 0, sizeXL = false, readOnly = false}) => {  
+const InputForm = ({value="",setter= (e)=>{},label, type = "text",cambiar = false, select = [] , money=false, sizeH = "50px",inputW = "100%", marginB = 5, marginT = 0, sizeXL = false, readOnly = false}) => {  
+ 
   return (
 <Box display="flex" flexDirection={"column"} mb={marginB} mt={marginT} width={inputW} justifyContent={"flex-start"}  >
     <label style={{color: "#475367", fontWeight: 500, marginBottom: 2}} htmlFor={"input-"+label}>{label}</label>
@@ -29,6 +30,7 @@ const InputForm = ({value="",setter,label, type = "text",cambiar = false, select
         </Select>
         :
         <OutlinedInput
+       
         
           readOnly= {readOnly}
           value={value}
