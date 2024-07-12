aafac3faaba35dbb5ada89971c5f64ec0a2478c1
brianmunioz <brianmunozjob@gmail.com>
2024-07-12 15:00:57 -0300

    Implementación de perfil de administrador terminado

--- src/Pages/PageAdminPerfil.jsx
+++ src/Pages/PageAdminPerfil.jsx
@@ -1,12 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Avatar, Button, Stack, useMediaQuery } from "@mui/material";
+import { Alert, Avatar, Button, Snackbar, Stack, useMediaQuery } from "@mui/material";
 import InputForm from "../Components/inputs/InputForm";
-
+import { getUser, patchBasicUserData } from "../Redux/Actions/UserActions/userActions";
+import Cookies from 'js-cookie'
 const PageAdminPerfil = () => {
   const mobile = useMediaQuery("(max-width: 750px)");
   const dispatch = useDispatch();
   const [editar, setEditar] = useState(false);
+  const [dataChanged, setDataChanged] = useState(false);
+
+  const [errorValidation, setErrorValidation] = useState({ value: false, message: "Mensaje incorrecto" });
+  const { user } = useSelector(state => state.user);
+  const [data, setData] =useState(user);
+
+  React.useEffect(() => {
+
+    dispatch(getUser(Cookies.get("id")));
+    setData(user);
+    setEditar(false);
+  }, []);
+ useEffect(() => {
+    if (!editar) {
+      setData(user);
+    }
+
+  }, [user])
+
+  const putBasicData = () => {
+    const regex = /^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]+$/;
+    if (!data.name) {
+      setErrorValidation({ value: true, message: "El campo nombre debe completarse" })
+    } else if (!data.lastname) {
+      setErrorValidation({ value: true, message: "El campo apellido debe completarse" })
+
+    } else {
+
+      if (regex.test(data.name) == false) {
+        setErrorValidation({ value: true, message: "Su nombre no puede contener carácteres especiales ni números" })
+
+      } else if (regex.test(data.lastname) == false) {
+        setErrorValidation({ value: true, message: "Su apellido no puede contener carácteres especiales ni números" })
+
+      } else {
+ setEditar(false)
+              setDataChanged(true)
+                dispatch(patchBasicUserData(data.name, data.lastname))
+              }
+             
+            }
+       
+  }
+
+
   return (
     <>
       <Stack direction="row" justifyContent={"center"}>
@@ -45,6 +91,13 @@ const PageAdminPerfil = () => {
             </label>
           </div>
           <InputForm
+          value={data.name} 
+          setter={(valor) => {
+            setData(prevState => ({
+              ...prevState,
+              name: valor
+            }))
+          }}
             label="Nombre"
             sizeH="35px"
             marginT={3}
@@ -52,6 +105,13 @@ const PageAdminPerfil = () => {
             readOnly={!editar}
           />
           <InputForm
+          value={data.lastname} 
+          setter={(valor) => {
+            setData(prevState => ({
+              ...prevState,
+              lastname: valor
+            }))
+          }}
             label="Apellido"
             sizeH="35px"
             marginB={3}
@@ -59,15 +119,18 @@ const PageAdminPerfil = () => {
           />
 
           <InputForm
+          value={user.email} 
             label="Correo electrónico"
             type="email"
             sizeH="35px"
             marginB={3}
-            readOnly={!editar}
+            readOnly={true}
           />
 
           {editar ? (
-            <Button variant="contained" style={{ fontWeight: "bold" }}>
+            <Button variant="contained" style={{ fontWeight: "bold" }}
+            onClick={()=>putBasicData()}
+            >
               {" "}
               Guardar Cambios
             </Button>
@@ -83,6 +146,27 @@ const PageAdminPerfil = () => {
           )}
         </Stack>
       </Stack>
+
+      <Snackbar open={errorValidation.value} anchorOrigin={{ vertical: "top", horizontal: "center" }} autoHideDuration={6000} onClose={() => setErrorValidation({ value: false, message: "" })}>
+        <Alert
+          onClose={() => setErrorValidation({ value: false, message: "" })}
+          severity="error"
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          {errorValidation.message}
+        </Alert>
+      </Snackbar>
+      <Snackbar open={dataChanged} anchorOrigin={{ vertical: "bottom", horizontal: "center" }} autoHideDuration={6000} onClose={() => setDataChanged(false)}>
+        <Alert
+          onClose={() => setDataChanged(false)}
+          severity="success"
+          variant="filled"
+          sx={{ width: '100%' }}
+        >
+          Los cambios se guardaron con éxito!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
