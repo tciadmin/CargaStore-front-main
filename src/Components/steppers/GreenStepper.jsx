2a4b749f82c3c5bbbc395fb9594df6993dc6fab4
KiuraAdmin <149433315+KiuraAdmin@users.noreply.github.com>
2024-09-16 12:38:32 -0500

    Update GreenStepper.jsx
    fix stepper

--- src/Components/steppers/GreenStepper.jsx
+++ src/Components/steppers/GreenStepper.jsx
@@ -2,22 +2,36 @@ import Box from '@mui/material/Box';
 import StepItem from './StepItem';
 import StepTrail from '../svg/StepTrail';
 
- const  GreenStepper = ({steps, activeStep = 2}) => {
+const GreenStepper = ({ steps, activeStep = 2 }) => {
   return (
-    <Box sx={{ width: '90%' }} display={"flex"} alignItems={"start"} justifyContent="space-evenly ">
-        {steps.map((label, index) => 
-          {
-          if(steps.length -1 == index){
-            return <StepItem description= {label} active = {index <= activeStep ? true : false} />
-     
-
-          }else{
-            return <><StepItem description= {label} active = {index  <= activeStep   ? true : false} /> <StepTrail  active={index + 1 <= activeStep  ? true : false}></StepTrail> </>
-          }
-          }
-        )} 
+    <Box sx={{ width: '90%' }} display="flex" alignItems="start" justifyContent="space-evenly">
+      {steps.map((label, index) => {
+        if (steps.length - 1 === index) {
+          return (
+            <StepItem
+              key={`step-item-${index}`} // Clave única para StepItem
+              description={label}
+              active={index <= activeStep}
+            />
+          );
+        } else {
+          return (
+            <React.Fragment key={`step-group-${index}`}>
+              <StepItem
+                key={`step-item-${index}`} // Clave única para StepItem
+                description={label}
+                active={index <= activeStep}
+              />
+              <StepTrail
+                key={`step-trail-${index}`} // Clave única para StepTrail
+                active={index + 1 <= activeStep}
+              />
+            </React.Fragment>
+          );
+        }
+      })}
     </Box>
   );
-}
+};
 
-export default GreenStepper
+export default GreenStepper;
