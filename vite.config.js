387344134da5f18f2733b75a3eff6dee80a1661e
Facu-J-Boy <boyfacundo45@gmail.com>
2024-12-06 12:13:24 -0300

    vite.config settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const urlBack = import.meta.env.VITE_URL_BACKEND;
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const urlBack = process.env.VITE_URL_BACKEND;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +16,6 @@ export default defineConfig({
         target: urlBack,
         ws: true,
       },
-    }
-  }
-})
+    },
+  },
+});
