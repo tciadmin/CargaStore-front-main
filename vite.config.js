1ecc368fe5c2834a1fac168034a5f6fea0414f02
Facu-J-Boy <boyfacundo45@gmail.com>
2024-12-05 22:36:18 -0300

    env settings

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const urlBack = import.meta.env.VITE_URL_BACKEND;
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: urlBack,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/socket.io': {
+          target: env.VITE_URL_BACKEND,
+          ws: true,
+        },
       },
-    }
-  }
-})
+    },
+  };
+});
